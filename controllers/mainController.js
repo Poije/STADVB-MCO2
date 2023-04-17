23015bd066927c47ae6a808dfc266ec1df3f2f3e
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2023-04-17 19:18:31 +0800

    fixed routing to other pages

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,7 +4,8 @@ const mainController = {
     },
 
     getEdit: (req, res) => {
-        res.render ("edit");
+        console.log (req.query);
+        res.render ("editMovie", {id: req.query.id, name: req.query.name, year: req.query.year, rank: req.query.rank});
     },
 
     getAdd: (req, res) => {
@@ -14,6 +15,12 @@ const mainController = {
     showVals: (req, res) => {
         console.log ("movie details: ");
         console.log (req.body);
+    },
+
+    renderSearch: (req, res) => {
+        res.render ('partials/searchResults', {results: req.body.results}, (err, html) => {
+            res.send (html);
+        });
     }
 };
 
--- routes/routes.js
+++ routes/routes.js
@@ -9,8 +9,12 @@ app.get ('/', mainController.getMainPage);
 
 app.get ('/editMovie', mainController.getEdit);
 app.get ('/addMovie', mainController.getAdd);
+app.post ('/renderSearch', mainController.renderSearch);
 
-app.get ('/test', dbController.select)
+app.get ('/test', dbController.select);
+app.post ('/showVals', mainController.showVals);
+
+app.post ('/replicate', dbController.replicate);
 app.post ('/select', dbController.select);
 app.post ('/insert', dbController.insert);
 app.post ('/update', dbController.update);
